fbf53fb85e4080ccb58b22606c5e6ba771c4f8b3
Trạch Huỳnh <trachhuynh@Trachs-MacBook-Pro.local>
2022-05-11 19:13:25 +0700

    Added files and changes required for deployment to heroku

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.use(('/', require('./routes/index')));
 app.use(('/api/url', require('./routes/url')));
 
 
-const PORT = 3000;
+const PORT = 443;
 
 app.listen(PORT, () => console.log(`Started server on ${PORT}`));
