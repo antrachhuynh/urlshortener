209288cf1f566ae075bd87ab7192f11e7dac6702
Trạch Huỳnh <trachhuynh@Trachs-MacBook-Pro.local>
2022-05-11 21:35:00 +0700

    Added helmet  and cors settings

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const helmet = require('helmet')
 const connectDb = require('./config/database');
 const app = express();
+const cors=require("cors");
+//secure the api
+app.use(helmet());
+
+
+
+
+//custom cors settings
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
+app.use(cors(corsOptions))
 
 
 //conect to mongodb
 
 connectDb();
+
 
 app.use(express.json({extended: false}))
 
